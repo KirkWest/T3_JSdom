bdc3270029517c41a34b2345d4bf9a16236e2be7
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-08-29 20:48:49 +1000

    JSdom web page start
    started webpage with html js jsthem and css created

---
+++ themeControl.js
@@ -0,0 +1,28 @@
+const themes = {
+  light: {
+    background: "white",
+    text:"darkblue"
+  },
+  dark: {
+    background: "grey",
+    text: "white"
+  }
+}
+
+function changeCSSTheme(themeName){
+    for (const variable in themes[themeName]) {
+    document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+  }
+}
+
+function toggleLightDark(){
+	let currentBackgroundColour = getComputedStyle(document.documentElement).getPropertyValue("--background");
+	if (currentBackgroundColour == "white") {
+		changeCSSTheme("dark");
+	}  else {
+		changeCSSTheme("light");
+	}
+}
+
+// changeCSSTheme("dark");
+// themes["light"]
